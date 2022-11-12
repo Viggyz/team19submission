4f528a81e31109c418e541b33bbf2169c027b889
Kaushik Deka <kaushik12dkrikhanu@gmail.com>
2022-11-12 22:23:10 +0400

    add map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import maplibregl from 'maplibre-gl';
 function App() {
   const [count, setCount] = useState(0)
-
+  useEffect(()=>{
+    const map = new maplibregl.Map({
+      container: 'map',
+      style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL', // stylesheet location
+      center: [75, 20], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+      });
+    
+  },[])
   return (
-    <div className="App">
-     
+    <div className="map" id="map" style={{height: "100vh", width: "100vw"}}>
+
     </div>
   )
 }
