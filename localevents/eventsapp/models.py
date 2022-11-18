6decd42f70c8ff9a529d4b7ce22a4b58a37575f8
Viggyz <devvig19@gmail.com>
2022-11-18 21:09:32 +0400

    Add models and endpoints

--- localevents/eventsapp/models.py
+++ localevents/eventsapp/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
-# Create your models here.
+from localevents.regex import osm_type_id_regex
+
+validate_id = []
+
+class Location(models.Model):
+    osm_type_id = models.CharField(
+        max_length=11, 
+        unique=True, 
+        db_index=True,
+        validators=[
+            RegexValidator(osm_type_id_regex)
+        ]
+    )
+    name = models.CharField(max_length=255)
+    place = models.CharField(max_length=255)
+    address = models.JSONField()
+    description = models.TextField()
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_events')
+    
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(blank=True, null=True)
+    
+    description = models.TextField(blank=True, null=True)
+    # image = models.FileField()
+    
+    interested = models.ManyToManyField(
+        User,
+        related_name='interested_events'
+    ) 
+    max_people = models.IntegerField()
+    current_people = models.IntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(current_people__lt=models.F("max_people")),
+                name="Current people already in are less than max"
+            ),
+            models.CheckConstraint(
+                check=models.Q(end_time__gt=models.F("start_time")),
+                name="End time must be less than start time"
+            ),
+            models.CheckConstraint(
+                check=(models.Q(max_people__gte=1) & models.Q(max_people__lte=12)),
+                name="Max people for an event can only be upto 11"
+            ),  
+        ]
+    
+    @classmethod
+    def create(cls, )
--- localevents/eventsapp/serializers.py
+++ localevents/eventsapp/serializers.py
@@ -1,14 +1,60 @@
+from django.db import transaction
 from django.contrib.auth.models import User
 
 from rest_framework import serializers
 
+from localevents.regex import osm_type_id_regex
+
+from .models import Location, Event
+
 class LocationListSerializer(serializers.Serializer):
-    xid = serializers.CharField()
+    # xid = serializers.CharField()
     # name = serializers.CharField(allow_null=True)
     kinds = serializers.CharField()
     rate = serializers.IntegerField()
     dist = serializers.FloatField()
     point = serializers.JSONField()
+
+class LocationSerializer(serializers.ModelSerializer): 
+    osm_type_id = serializers.RegexField(regex=osm_type_id_regex)
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+class EventListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+class EventSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    start_time = serializers.DateTimeField()
+    end_time = serializers.DateTimeField()
+    description = serializers.CharField(max_length=512)
+    max_people = serializers.IntegerField(max_value=12, min_value=1)
+    created_by = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    location = LocationSerializer(write_only=True)
+    
+    def create(self, validated_data):
+        location = None
+        location_details = validated_data.pop('location')
+        try:
+            location = Location.objects.get(osm_type_id=location_details.osm_type_id)
+        except Location.DoesNotExist: 
+            location = None
+            # TODO Prob a try catch
+        with transaction.atomic():
+            if not location:
+                location = Location.objects.create(**location_details)
+            event = Event.objects.create(**validated_data, location=location)
+        return event
+        
+
+    def validate(self, data):
+        if data["end_time"] > data["start_time"]:
+            raise serializers.ValidationError("An event cannot end before it begins")
+        return data
 
 class SignUpSerializer(serializers.Serializer):
     username = serializers.CharField(required=True, min_length=4, write_only=True)
@@ -18,3 +64,4 @@ class SignUpSerializer(serializers.Serializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
