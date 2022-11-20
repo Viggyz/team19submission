74d582de17b588e85af87f2b45cc541d86c55c9c
Viggyz <devvig19@gmail.com>
2022-11-20 15:03:15 +0400

    Uncomment event modal

--- src/components/addEventForm.jsx
+++ src/components/addEventForm.jsx
@@ -2,30 +2,27 @@ import React from 'react'
 import {Link, Modal, Paper,Box, TextField, Button, Typography} from "@mui/material";
 
 function AddEventForm({openEventModal, handleEventClose}) {
-
-  
-    // <Modal
-    // open={openEventModal}
-    // onClose={handleEventClose}>
-    //   <Box>
-    //     <Paper 
-    //         style={{
-    //             position: 'absolute',
-    //             top: '50%',
-    //             left: '50%',
-    //             transform: 'translate(-50%, -50%)',
-    //             width: 400,
-    //             // bgcolor: 'background.paper',
-    //             // border: '2px solid #000',
-    //             boxShadow: 24,
-    //             padding: '1rem',
-    //         }}
-    //     >
+    <Modal
+    open={openEventModal}
+    onClose={handleEventClose}>
+      <Box>
+        <Paper 
+            style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: 400,
+                // bgcolor: 'background.paper',
+                // border: '2px solid #000',
+                boxShadow: 24,
+                padding: '1rem',
+            }}
+        >
           
-    //     </Paper>
-    //   </Box>
-    // </Modal>
-  
+        </Paper>
+      </Box>
+    </Modal>
 }
 
 export default AddEventForm
