81fefd10b2a11ae86f3ba98e222b1a591f9a0de2
KiranbaskarV <kiran8402@gmail.com>
2022-11-22 22:04:45 +0400

    Update Event Detail Modal to show details

--- src/components/EventDetailModal.jsx
+++ src/components/EventDetailModal.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import moment from 'moment/moment';
+
 import {
   Modal, 
   Paper,
@@ -9,14 +11,45 @@ import {
   Typography,
   ButtonGroup,
   IconButton,
-  ListItemButton
+  ListItemButton,
+  Divider,
+  Chip
 } from "@mui/material";
 
 import { Events } from '../api.service';
 
 function EventDetailModal({openEventDetailModal, handleEventDetailModalClose, eventDetailID}) {
 
-    
+    const [eventDetails, setEventDetails] = React.useState({
+        name: "",
+        start_time: "",
+        end_time: "",
+        description: "",
+        max_people: 1,
+        interested: [],
+        current_people: 0,
+        created_by: {
+            username: "",
+            first_name: "",
+            last_name: "",
+            email: ""
+        },
+        location: {
+            id: 4,
+            osm_type_id: "",
+            name: "",
+            place: "",
+            address: {
+                name: "",
+                country: "",
+                country_code: ""
+            }
+        }
+    })
+
+    Events.get(eventDetailID).then(({data}) => {
+        setEventDetails(data);
+    })
 
     return(
         <Modal
@@ -33,7 +66,16 @@ function EventDetailModal({openEventDetailModal, handleEventDetailModalClose, ev
                 padding: '1rem',
             }}
             >
-                <Typography variant='h2'></Typography>
+                <Typography variant='h3'>{eventDetails.name}</Typography>
+                <Typography>{moment(eventDetails.start_time).format("Mo MMM hh:mm A")} to {moment(eventDetails.end_time).format("Mo MMM hh:mm A")}</Typography>
+                <Typography variant='body1'>Created By: {eventDetails.created_by.username}</Typography>
+
+                <Divider sx={{my:'1em'}} />
+                <Chip variant='filled' color='info' label={`${eventDetails.current_people} / ${eventDetails.max_people}`}
+                sx={{mb:'1em'}}/>
+
+                <Typography variant='body1'>  {eventDetails.description} </Typography>
+                
             </Paper>
         </Modal>
     )
